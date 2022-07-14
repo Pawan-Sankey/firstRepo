910db9966a90c9e2a17dd3471536e3decc38c03f
Pawan-Sankey <pawan.n@sankeysolutions.com>
2022-07-14 12:42:29 +0530

    Day 3 task

---
+++ hello.py
@@ -0,0 +1,13 @@
+from tokenize import String
+
+
+print("Hello from Python Interpreter!")
+
+class Book:
+    id = 1
+    author = "Unknown"
+    copies_sold = 23
+
+b1 = Book
+print(b1.author)
+print(b1.copies_sold)
